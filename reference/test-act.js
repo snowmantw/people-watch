ee5b2960ad9d8a8df4d13ddcff5a6194bad599bc
snowmantw <snowmantw@gmail.com>
2013-06-07 18:55:17 +0800

    Remove tmp files

--- reference/test-act.js
+++
@@ -1,5 +0,0 @@
-act = {'from': 'Greg Weng', 'subject': 'Sent a message to you', 'star_activitied': true}
-con = {'from':'Greg Weng', 'message': 'Some message...'}
-e = {'name':'new-activity', 'type': 'message', data: {'activity': act, 'content': con}}
-Notifier.trigger(e)
-
